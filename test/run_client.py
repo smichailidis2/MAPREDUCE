3dc1c4d63120df6b2185a697c87a2b28d48859d3
BrickOyster <dimitris2002keramidas@gmail.com>
2024-06-28 11:04:50 +0300

    fix some master node errors

--- test/run_client.py
+++ test/run_client.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 BASE_URL_MASTER = 'http://master-service.sad.svc.cluster.local:5000'
-BASE_URL_AUTH = 'http://flask-pod-service.sad.svc.cluster.local:5001'
+BASE_URL_AUTH = 'http://flask-app-service.sad.svc.cluster.local:5001'
 
 def register_user(username, password):
     url = f"{BASE_URL_AUTH}/register"
@@ -20,12 +20,12 @@ def login_user(username, password):
     response = requests.post(url, data=json.dumps(payload), headers=headers)
     return response
 
-def create_jobs(token, job_type, num_jobs):
-    url = f"{BASE_URL_MASTER}/create_jobs"
-    payload = {'mapper_num': job_type, 'reducer_num': num_jobs}
+def create_jobs(map, red):
+    url = f"{BASE_URL_MASTER}/submit_job"
+    payload = {'mapper_num': map, 'reducer_num': red}
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {123}'
     }
     
     response = requests.post(url, data=json.dumps(payload), headers=headers)
@@ -35,16 +35,16 @@ if __name__ == '__main__':
     username = 'testuser'
     password = 'testpass'
     
-    # Register user
-    register_response = register_user(username, password)
-    print('Register Response:', register_response.json())
+    # # Register user
+    # register_response = register_user(username, password)
+    # print('Register Response:', register_response.json())
     
-    # Login user
-    login_response = login_user(username, password)
-    print('Login Response:', login_response.json())
+    # # Login user
+    # login_response = login_user(username, password)
+    # print('Login Response:', login_response.json())
     
-    token = login_response.json().get('token')
+    # token = login_response.json().get('token')
     
     # Create jobs
-    job_response = create_jobs(token, 'mapper', 3)
+    job_response = create_jobs(3, 2)
     print('Create Jobs Response:', job_response.json())
